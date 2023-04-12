ccb5c14b072683a39248f876a9c79f11f1d5236a
0kkun <shinji.okumoto.01@gmail.com>
2023-04-13 07:44:24 +0900

    APIのエラーハンドリング実装

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -4,10 +4,11 @@ namespace App\Exceptions;
 
 use App\Http\Resources\Common\ErrorResource;
 use App\Http\Resources\Common\InvalidResource;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
-use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -47,27 +48,45 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e, $request) {
-
-            // if ($request->is('api/*')) {
-            //     if ($e instanceof AccessDeniedException) {
-            //         return new ErrorResource([
-            //             'errors' => [
-            //                 'message' => 'This action is unauthorized.',
-            //             ], Response::HTTP_UNAUTHORIZED
-            //         ]);
-            //     }
-            //     if ($e instanceof ValidationException) {
-            //         return new InvalidResource($e->errors());
-            //     }
-            //     return new ErrorResource([
-            //         'errors' => [
-            //             'message' => $e->getMessage(),
-            //             'class' => get_class($e),
-            //             'file' => $e->getFile(),
-            //             'line' => $e->getLine(),
-            //         ]
-            //     ]);
-            // }
         });
+    }
+
+    /**
+     * APIの場合はエラー画面ではなく、JSONでエラーコードとメッセージを返す
+     *
+     * @param [type] $request
+     * @param Throwable $exception
+     * @return void
+     */
+    public function render($request, Throwable $e)
+    {
+        if ( $request->is('api/*') ) {
+            if ($e instanceof AuthorizationException) {
+                $messages = config('api_response.messages');
+                return new ErrorResource([
+                    $messages[Response::HTTP_UNAUTHORIZED]
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+            if ($e instanceof ValidationException) {
+                return new InvalidResource($e->errors());
+            }
+            if ($e instanceof HttpException) {
+                return new ErrorResource([
+                    'message' => $e->getMessage(),
+                    'class' => get_class($e),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ], $e->getStatusCode());
+            }
+            return new ErrorResource([
+                'errors' => [
+                    'message' => $e->getMessage(),
+                    'class' => get_class($e),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine(),
+                ]
+            ]);
+        }
+        return parent::render($request, $e);
     }
 }
